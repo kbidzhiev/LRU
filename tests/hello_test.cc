cad67f1b66f7edc9fdf495dd3222330501135d43
Your Name <you@example.com>
2026-06-20 16:56:24 +0200

    working tests

--- tests/hello_test.cc
+++ tests/hello_test.cc
@@ -1,9 +1,8 @@
+#include "hello.hpp"
 #include <gtest/gtest.h>
 
 // Demonstrate some basic assertions.
 TEST(HelloTest, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
   // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+  EXPECT_EQ(hello(), std::string("Hello, world!"));
 }
